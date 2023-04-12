e6a259bd071f5afedfa4df8554ec1e67011149d4
Matheus Negrini <mndias29@gmail.com>
2023-04-12 16:49:52 -0300

    estruturei o negocio e já meti a header slc mlk sabe d+

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import './index.css'
+
+function Header() {
+    return ( 
+        <div className='header'>
+            <div className='container'>
+                <h1>MNFlix</h1>
+                <div className='nav'>
+                    <a>Home</a>
+                    <a>Lorem</a>
+                    <a>Ipsum</a>
+                    <a>Kekw</a>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './assets/style/index.css'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Header />
+  </React.StrictMode>
 )
