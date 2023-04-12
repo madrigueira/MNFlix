403f0acd2fb9580db7918c57aaebb76698e3e06f
Matheus Negrini <mndias29@gmail.com>
2023-04-12 17:48:47 -0300

    ai ai a header ta pronta papai

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,10 @@
 import './index.css'
+import { AiOutlineMenu } from 'react-icons/ai';
+
+function openMenu(){
+    const menu = document.querySelector('.nav')
+    menu.classList.toggle('active')
+}
 
 function Header() {
     return ( 
@@ -6,11 +12,13 @@ function Header() {
             <div className='container'>
                 <h1>MNFlix</h1>
                 <div className='nav'>
+                    <button className='close mobile' onClick={openMenu}>✕</button>
                     <a>Home</a>
                     <a>Lorem</a>
                     <a>Ipsum</a>
                     <a>Kekw</a>
                 </div>
+                <button className="menu mobile" onClick={openMenu}><AiOutlineMenu /></button>
             </div>
         </div>
      );
