d96a724f38ae4356d782ea7ea803eeb6ee9ae9ae
Matheus Negrini <mndias29@gmail.com>
2023-04-16 21:28:52 -0300

    playlist de sexo do rafaelkkkkkkkkkkk

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,25 +1,28 @@
-import './index.scss'
-import { BsFillPlayFill } from 'react-icons/bs';
-import { AiOutlineInfoCircle } from 'react-icons/ai';
+ import './index.scss'
+ import { BsFillPlayFill } from 'react-icons/bs';
+ import { AiOutlineInfoCircle } from 'react-icons/ai';
+ import data from '../../dados.json';
 
-function Banner({bg}){
-  return (
-    <div className='banner'style={{backgroundImage:`url('${bg}')`}}>
-        <div className="container">
-            <h3>Best of <br/> Calegão</h3>
-            <div className="top">
-                <a>top<br/>10</a>
-                <h4>Top 1 em sonegação hoje</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus temporibus distinctio repellendus, 
-            ducimus, nisi nemo cumque id.</p>
-            <div className="buttons">
-                <button className="assistir"><BsFillPlayFill/>Assistir</button>
-                <button className="informacoes"><AiOutlineInfoCircle/>Mais Informações</button>
-            </div>
-        </div>
-    </div>
-  )
-}
+ function Banner(){
+  const {bg,title} = data.teste[Math.floor(Math.random() * data.teste.length)]
 
-export default Banner
+   return (
+   <div className='banner'style={{backgroundImage:`url(${bg})`}}>
+         <div className="container">
+             <h3 className={title.length > 36 ? 'small' : ""}>{title}</h3>
+             <div className="top">
+                 <a>top<br/>10</a>
+                 <h4>Top 1 em sonegação hoje</h4>
+             </div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus temporibus distinctio repellendus, 
+             ducimus, nisi nemo cumque id.</p>
+             <div className="buttons">
+                 <button className="assistir"><BsFillPlayFill/>Assistir</button>
+                 <button className="informacoes"><AiOutlineInfoCircle/>Mais Informações</button>
+             </div>
+         </div>
+     </div>
+   )
+ }
+
+ export default Banner
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import Banner from './components/Banner'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <Banner bg="https://i.ytimg.com/vi/oAMVSi_pkwc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-DoACuAiKAgwIABABGFkgQyhlMA8=&rs=AOn4CLBq6MvbIDo3HuYPxiPh2USEIBOf0w"/>
+    <Banner />
   </React.StrictMode>
 )
